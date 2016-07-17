84c7704314c7be9b1099dfc45a7ced490823c675
VivienShaw <516090566@qq.com>
2016-07-17 22:12:34 +0800

    complete the plus method

---
+++ src/main/java/Compute.java
@@ -0,0 +1,8 @@
+public class Compute {
+
+    public double plus(double m, double n) {
+        double result;
+        result = m + n;
+        return result;
+    }
+}
---
+++ src/test/java/ComputeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComputeTest {
+
+    @Test
+    public void should_return_2_when_input_1_and_1() throws Exception {
+        Compute compute = new Compute();
+        assertEquals(2,compute.plus(1,1),0.01);
+    }
+}
